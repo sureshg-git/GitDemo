365ddf0c01b61ec08c83068df3729cff8b53bb30
Suresh <suresh.work85@gmail.com>
2021-01-15 16:53:12 +0530

    this commit from y user

--- src/test/java/automation/MavenJava/SeleniumTest.java
+++ src/test/java/automation/MavenJava/SeleniumTest.java
@@ -51,6 +51,27 @@ public class SeleniumTest {
 		driver.quit();
 
 	}
+	
+	@Test(dataProvider = "adminData")
+	public void adminLogin(String username, String pw) {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\govindarajs\\Documents\\chromedriver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.get("https://qatest.eschoolsolutions.com/");
+		;
+		WebElement userid = driver.findElement(By.id("userId"));
+		WebElement password = driver.findElement(By.id("userPin"));
+		userid.sendKeys(username);
+		password.sendKeys(pw);
+		driver.findElement(By.id("submitBtn")).click();
+		driver.findElement(By.id("dialog_close-button")).click();
+		System.out.println(driver.getTitle());
+		driver.findElement(By.id("sidious.prompt.header.signOut")).click();
+		driver.quit();
+
+	}
 
 	@DataProvider
 	public Object empData() {
@@ -64,6 +85,14 @@ public class SeleniumTest {
 	public Object subData() {
 		Object[][] data = new Object[1][2];
 		data[0][0] = "7012";
+		data[0][1] = "Password@7010";
+		return data;
+	}
+	
+	@DataProvider
+	public Object adminData() {
+		Object[][] data = new Object[1][2];
+		data[0][0] = "7013";
 		data[0][1] = "Password@7010";
 		return data;
 	}
